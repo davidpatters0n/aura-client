72feba2797c32c69b3a257134469c3a6e6adbae5
David Patterson <dpatterson2008@hotmail.com>
2017-05-30 20:16:31 -0400

    Move App component into src/components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
+
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <Route exact path="/" component={App}></Route>
+      </div>
+    </ConnectedRouter>
+  </Provider>
+);
+
+ReactDOM.render(router, document.getElementById('root'));
 registerServiceWorker();
