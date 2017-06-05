e334b21082b8294fc1c9924fc3818f0038c39bfc
David Patterson <dpatterson2008@hotmail.com>
2017-06-04 20:21:59 -0400

    Set up App component to include GoogleMap & SearchContainer - Also wire up to redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,61 @@
+/* eslint-disable no-undef */
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/app';
+
+// Components
+import GoogleMap from './google-map';
+import SearchContainer from './search-container';
+
+// Styles
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.loadAtms();
+    this.handleSearchUpdate = this.handleSearchUpdate.bind(this)
+  }
+
+  handleSearchUpdate(term) {
+    console.log('Todo');
+  }
+
   render() {
+    let map = <div>Loading...</div>
+    const { isLoading, atms, longitude, latitude } = this.props;
+
+    if (!isLoading && longitude && latitude) {
+      map = <GoogleMap latitude={latitude} longitude={longitude} atms={atms}/>
+    }
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className='google-map-container'>
+        <div className='google-map-container__row row'>
+          <div className='col-md-6 google-map-container-map'>
+            {map}
+          </div>
+          <div className="col-md-6">
+            <SearchContainer onSearchUpdate={this.handleSearchUpdate} />
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    atms: state.app.atms,
+    isLoading: state.isLoading,
+    latitude: state.location.latitude,
+    longitude: state.location.longitude
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(actionCreators, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
